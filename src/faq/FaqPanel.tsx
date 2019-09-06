f72122a79ee5c0aced48749c28b810d5dde2cb8b
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-09-06 23:54:21 +0300

    Add FAQ content

--- src/faq/FaqPanel.tsx
+++ src/faq/FaqPanel.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const FaqPanel: React.FC = () => {
   return (
     <Container>
-      <p>FAQ!</p>
+      <Row>
+        <Col>
+          <h3>Mikä tämä on?</h3>
+          <p>
+            Hauska peli jonka tein tyypatessa <a href="https://nestjs.com/">Nest.js</a>:ää. Vaikuttaa ihan hauskalta freimikseltä.
+          </p>
+          <hr />
+          <h3>Miten tämä toimii?</h3>
+          <p>
+            Helposti: arvotaan ensin kysymys ja siihen kaksi yritystä. Käyttäjä valitsee yrityksen joka vastaa kysymykseen hänen mielestä paremmin. Pisteet lasketaan kysymyksittäin käyttäen vanha kunnon (Clanbasesta tuttua) <a href="https://en.wikipedia.org/wiki/Elo_rating_system">ELO-lukua</a>.
+          </p>
+          <hr />
+          <h3>Miten saan yritykseni mukaan?</h3>
+          <p>
+            Heitä ehdotus yrityksestä minulle meilitse: <a href="mailto:teemu@talented.fi?subject=Uusi%20yritys%20Firma-battleen&body=Heippa%2C%0A%0Alis%C3%A4tk%C3%A4%C3%A4%20t%C3%A4m%C3%A4%20yritys%20Firma-battleen%3A%0A%0AYritys%3A%20%0AKotisivu%3A%20%0A%0AKiitos!">teemu@talented.fi</a>.
+          </p>
+        </Col>
+      </Row>
     </Container>
   )
 };
