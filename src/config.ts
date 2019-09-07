ffd72c88c57ec6638b5ceb8cfb7b0749246ded2f
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-09-07 12:41:13 +0300

    Update api url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export default {
-  api_endpoint: 'http://localhost:3000',
-  image_endpoint: 'http://localhost:3000/images'
+  api_endpoint: 'https://firma-battle-api.herokuapp.com',
+  image_endpoint: 'https://firma-battle-api.herokuapp.com/images'
 };
