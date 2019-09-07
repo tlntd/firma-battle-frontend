a6e0040a23c628525ac7d45c48211aa73dde910f
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-09-07 09:29:54 +0300

    Updates

---
+++ precache-manifest.4f9548946dbc6c8d187a575e95ef6d46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9aa7c2864e43d4698e075f2353e45c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c178a573dfb1838af9a4",
+    "url": "/static/css/main.b4aacb73.chunk.css"
+  },
+  {
+    "revision": "45cedda75a95fe7ad632",
+    "url": "/static/js/2.3bc17c01.chunk.js"
+  },
+  {
+    "revision": "c178a573dfb1838af9a4",
+    "url": "/static/js/main.568511df.chunk.js"
+  },
+  {
+    "revision": "22c6d36bfcc2ba9c1b7b",
+    "url": "/static/js/runtime~main.18b2e3a4.js"
+  }
+]);
